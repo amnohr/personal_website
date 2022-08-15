338ac29808b7f9d9914539a116e4bc4c36e25b3b
Alf Magnus G Nohr <amnohr@stud.ntnu.no>
2022-08-15 14:52:50 +0200

    added projects section

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,45 @@ function About(){
     )
 }
 
+// Projects showcase
+function Projects(){
+    return(
+        <section className={`${styles.grid_container} ${styles.projects}`}>
+            <div className={styles.projects_header}>
+                <h3>Projects</h3>
+                <p>Some of my open source projects.</p>
+            </div>
+
+            <figure>
+                <a href="https://github.com/amnohr/personal_website" target="_blank" rel="noreferrer">
+                    <img src="/images/website_project.png" alt="link to github repo"/>
+                </a>
+                <p>This website. </p>
+                <p>Even if it's not advanced. It have been set up with React and Next.js for future improvements and learning</p>
+
+            </figure>
+
+            <figure>
+                <a href="https://github.com/amnohr" target="_blank" rel="noreferrer">
+                    <img src="/images/profile_picture.jpg" alt="link to github repo"/>
+                </a>
+                <p>Coming soon!</p>
+
+            </figure>
+
+            <figure>
+                <a href="https://github.com/amnohr" target="_blank" rel="noreferrer">
+                    <img src="/images/profile_picture.jpg" alt="link to github repo"/>
+                </a>
+                <p>Coming soon!</p>
+
+            </figure>
+
+
+        </section>
+    )
+}
+
 
 // Main index.js
 export default function Home(){
@@ -54,6 +93,7 @@ export default function Home(){
 
             <StartPage />
             <About />
+            <Projects />
 
         </main>
 
