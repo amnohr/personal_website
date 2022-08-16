dd5b8543e3ca306b1082c346c260e0d10b1f4a56
Alf Magnus G Nohr <amnohr@stud.ntnu.no>
2022-08-16 11:49:44 +0200

    test fix for iphone notch

--- pages/index.js
+++ pages/index.js
@@ -102,6 +102,7 @@ export default function Home(){
             <Head>
                 <title>Alf Magnus Nohr</title>
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
             </Head>
 
             <StartPage />
